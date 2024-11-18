72a3d5d7414c9bab4cc06b6bd41d9aef258e417e
Art San <artsan.mcc@gmail.com>
2024-11-18 23:42:59 +0700

    add dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@ import { HomeIcon, File, UsersRound } from 'lucide-react'
 import Link from 'next/link'
 
 import { NavButton } from '@/components/NavButton'
-// import { ModeToggle } from '@/components/ModeToggle'
+import { ModeToggle } from '@/components/ModeToggle'
 
 export function Header() {
   return (
     <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
       <div className="flex h-8 items-center justify-between w-full">
         <div className="flex items-center gap-2">
-          {/* <NavButton href="/home" label="Home" icon={HomeIcon} /> */}
+          <NavButton href="/home" label="Home" icon={HomeIcon} />
 
           <Link
             href="/home"
@@ -27,7 +27,7 @@ export function Header() {
 
           <NavButton href="/customers" label="Customers" icon={UsersRound} />
 
-          {/* <ModeToggle /> */}
+          <ModeToggle />
         </div>
       </div>
     </header>
